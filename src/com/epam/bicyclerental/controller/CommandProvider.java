f329277223f7b2216081daabed7ea33bb97d9aee
OSmol <Olga_Smolyakova@outlook.com>
2016-10-31 22:16:25 +0300

    Update CommandProvider.java

--- src/com/epam/bicyclerental/controller/CommandProvider.java
+++ src/com/epam/bicyclerental/controller/CommandProvider.java
@@ -11,7 +11,7 @@ class CommandProvider {
 	private Map<String, Command> commands = new HashMap<String, Command>();
 	
 	CommandProvider() {
-		commands.put("ADD_PRODUCT", new AddProduct());
+		commands.put("ADD_PRODUCT", new AddProduct());// целвй пакет констант создал, а про именования константных строк забыл
 		commands.put("DELETE_PRODUCT", new DeleteProduct());
 		commands.put("EDIT_PRODUCT", new EditProduct());
 		commands.put("SEARCH_ALL_PRODUCTS", new SearchAllProducts());
